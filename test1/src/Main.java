7b331a2b81fdbaeed6308f8ce1a36579057f5157
phuc <phucquan0106@gmail.com>
2019-01-19 19:31:16 +0700

    add Main1

---
+++ test1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public int heSo = 10;
+    public static int heSoMacDinh = 16;
+    public static void main(String[] args) {
+        Main x = new Main();
+        System.out.println("heSo = "+ x.heSo);
+        System.out.println("heSoMacDinh = "+Main.heSoMacDinh);
+    }
+}
