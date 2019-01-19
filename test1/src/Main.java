acef1b5a5908efe0ce026ffdc09724747304c14a
phuc <phucquan0106@gmail.com>
2019-01-19 19:58:59 +0700

    updt Main

--- test1/src/Main.java
+++ test1/src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public int heSo = 10;
-    public static int heSoMacDinh = 16;
+    public final static int heSoMacDinh = 16;
+    public enum season{muaXuan,muaHe,muaThu,muaDong};
     public static void main(String[] args) {
         Main x = new Main();
         System.out.println("heSo = "+ x.heSo);
+        //Main.heSoMacDinh = 15;
         System.out.println("heSoMacDinh = "+Main.heSoMacDinh);
+        System.out.println("Mua: "+season.muaDong);
     }
 }
