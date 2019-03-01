cd5b135271f67ccb5b10352513bca2c93cecf268
phuc <phucquan0106@gmail.com>
2019-03-01 19:03:14 +0700

    update main1 theo yeu cau

--- test1/src/ExamJava1/Main1.java
+++ test1/src/ExamJava1/Main1.java
@@ -1,4 +1,26 @@
 package ExamJava1;
+import java.util.Scanner;
 
-public class Main1 {
-}
+
+    public class Main1 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            Item item;
+            System.out.println("Please input item details: ");
+            System.out.println("itemId: ");
+            int itemId = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("name: ");
+            String name = scanner.nextLine();
+            System.out.println("Producer: ");
+            String producer = scanner.nextLine();
+            System.out.println("price: ");
+            double price = scanner.nextDouble();
+            scanner.nextLine();
+            item = new Item(itemId,name,producer,price);
+            System.out.println("Item details: "+ item.toString());
+        }
+
+
+    }
+
