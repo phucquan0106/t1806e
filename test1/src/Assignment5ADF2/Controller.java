f23ed7c2e9bec68cae663e89217e563f0f540d49
phuc <phucquan0106@gmail.com>
2019-03-16 21:34:25 +0700

    Demo FXML

--- test1/src/Assignment5ADF2/Controller.java
+++ test1/src/Assignment5ADF2/Controller.java
@@ -1,5 +1,54 @@
 package Assignment5ADF2;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
+
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public TableColumn<User,Integer> id;
+    public TableColumn<User,String> username;
+    public TableColumn<User,String> email;
+    public TableColumn<User,String> status;
+
+    public TableView tableView;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        id.setCellValueFactory(new PropertyValueFactory<User,Integer>("id"));
+        username.setCellValueFactory(new PropertyValueFactory<User,String>("username"));
+        email.setCellValueFactory(new PropertyValueFactory<User,String>("email"));
+        status.setCellValueFactory(new PropertyValueFactory<User,String>("status"));
+
+        Connector connector = new Connector();
+
+        String sqlQuery = "SELECT * FROM user";
+
+        try{
+            ResultSet rs = connector.getQuery(sqlQuery);
+
+            ObservableList<User> list = FXCollections.observableArrayList();
+
+            while (rs.next()){
+                Integer id = rs.getInt("id");
+                String name = rs.getString("username");
+                String email = rs.getString("email");
+                Integer status = rs.getInt("status");
+                User user = new User(id,name,email,status);
+                list.add(user);
+            }
+            tableView.setItems(list);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+    }
 }
---
+++ test1/src/Assignment5ADF2/User.java
@@ -0,0 +1,47 @@
+package Assignment5ADF2;
+
+public class User {
+    public Integer id;
+    public String username;
+    public String email;
+    public Integer status;
+
+    public User(int id, String username, String email, int status) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
